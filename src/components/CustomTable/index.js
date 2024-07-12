6dc59cf38961137d45e728dfe5510f0653797c59
Vasili <vasilisalda0528@outlook.com>
2024-07-12 10:19:16 +0400

    Add Bet table hover style

--- src/components/CustomTable/index.js
+++ src/components/CustomTable/index.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import BET_TABLE from "../../consts/BetTable";
+import {
+  BET_TABLE,
+  twoByOneFirst,
+  twoByOneSecond,
+  twoByOneThird,
+  firstTwelves,
+  secondTwelves,
+  thirdTwelves,
+  oneToEighteen,
+  nineteenToThirtySix,
+  even,
+  odd,
+  red,
+  black,
+} from "../../consts/BetTable";
 
 import "./styles.css";
 const CustomTable = () => {
+  const [numStates, setNumStates] = useState([]);
+
   return (
     <div className="betTable">
       <table border={1} cellPadding={1} cellSpacing={1} height={100}>
@@ -18,36 +34,175 @@ const CustomTable = () => {
               <div>22</div>
             </td>
             {BET_TABLE.firstRow.map((one) => (
-              <td className={`td-${one.color}`}>{one.label}</td>
+              <td
+                key={one.label}
+                className={`td-${one.color} ${
+                  numStates.includes(one.label) && "hover"
+                } `}
+              >
+                {one.label}
+              </td>
             ))}
-            <td className="td-row">2 to 1</td>
+            <td
+              className="td-row"
+              onMouseOver={() => {
+                setNumStates(twoByOneFirst);
+              }}
+              onMouseOut={() => {
+                setNumStates([]);
+              }}
+            >
+              2 to 1
+            </td>
           </tr>
           <tr>
             {BET_TABLE.secondRow.map((one) => (
-              <td className={`td-${one.color}`}>{one.label}</td>
+              <td
+                key={one.label}
+                className={`td-${one.color} ${
+                  numStates.includes(one.label) && "hover"
+                }`}
+              >
+                {one.label}
+              </td>
             ))}
-            <td className="td-row">2 to 1</td>
+            <td
+              className="td-row"
+              onMouseOver={() => {
+                setNumStates(twoByOneSecond);
+              }}
+              onMouseOut={() => {
+                setNumStates([]);
+              }}
+            >
+              2 to 1
+            </td>
           </tr>
           <tr>
             {BET_TABLE.thirdRow.map((one) => (
-              <td className={`td-${one.color}`}>{one.label}</td>
+              <td
+                key={one.label}
+                className={`td-${one.color} ${
+                  numStates.includes(one.label) && "hover"
+                }`}
+              >
+                {one.label}
+              </td>
             ))}
-            <td className="td-row">2 to 1</td>
+            <td
+              className="td-row"
+              onMouseOver={() => {
+                setNumStates(twoByOneThird);
+              }}
+              onMouseOut={() => {
+                setNumStates([]);
+              }}
+            >
+              2 to 1
+            </td>
           </tr>
           <tr>
             <td className="hidden-td"></td>
-            <td colSpan={4}>5</td>
-            <td colSpan={4}>5</td>
-            <td colSpan={4}>5</td>
+            <td
+              colSpan={4}
+              onMouseOver={() => {
+                setNumStates(firstTwelves);
+              }}
+              onMouseOut={() => {
+                setNumStates([]);
+              }}
+            >
+              1st 12
+            </td>
+            <td
+              colSpan={4}
+              onMouseOver={() => {
+                setNumStates(secondTwelves);
+              }}
+              onMouseOut={() => {
+                setNumStates([]);
+              }}
+            >
+              2nd 12
+            </td>
+            <td
+              colSpan={4}
+              onMouseOver={() => {
+                setNumStates(thirdTwelves);
+              }}
+              onMouseOut={() => {
+                setNumStates([]);
+              }}
+            >
+              3rd 12
+            </td>
           </tr>
           <tr>
             <td className="hidden-td"></td>
-            <td colSpan={2}>6</td>
-            <td colSpan={2}>6</td>
-            <td colSpan={2}>6</td>
-            <td colSpan={2}>6</td>
-            <td colSpan={2}>6</td>
-            <td colSpan={2}>6</td>
+            <td
+              colSpan={2}
+              onMouseOver={() => {
+                setNumStates(oneToEighteen);
+              }}
+              onMouseOut={() => {
+                setNumStates([]);
+              }}
+            >
+              1-18
+            </td>
+            <td
+              colSpan={2}
+              onMouseOver={() => {
+                setNumStates(even);
+              }}
+              onMouseOut={() => {
+                setNumStates([]);
+              }}
+            >
+              Even
+            </td>
+            <td
+              className="betTable-red-diamond"
+              colSpan={2}
+              onMouseOver={() => {
+                setNumStates(red);
+              }}
+              onMouseOut={() => {
+                setNumStates([]);
+              }}
+            ></td>
+            <td
+              className="betTable-black-diamond"
+              colSpan={2}
+              onMouseOver={() => {
+                setNumStates(black);
+              }}
+              onMouseOut={() => {
+                setNumStates([]);
+              }}
+            ></td>
+            <td
+              colSpan={2}
+              onMouseOver={() => {
+                setNumStates(odd);
+              }}
+              onMouseOut={() => {
+                setNumStates([]);
+              }}
+            >
+              Odd
+            </td>
+            <td
+              colSpan={2}
+              onMouseOver={() => {
+                setNumStates(nineteenToThirtySix);
+              }}
+              onMouseOut={() => {
+                setNumStates([]);
+              }}
+            >
+              19-36
+            </td>
           </tr>
         </tbody>
       </table>
--- src/consts/BetTable.js
+++ src/consts/BetTable.js
@@ -1,44 +1,70 @@
-export default {
+export const BET_TABLE = {
   firstRow: [
-    { label: "3", color: "red" },
-    { label: "6", color: "black" },
-    { label: "9", color: "red" },
-    { label: "12", color: "red" },
-    { label: "15", color: "black" },
-    { label: "18", color: "red" },
-    { label: "21", color: "red" },
-    { label: "24", color: "black" },
-    { label: "27", color: "red" },
-    { label: "30", color: "red" },
-    { label: "33", color: "black" },
-    { label: "36", color: "red" },
+    { label: 3, color: "red" },
+    { label: 6, color: "black" },
+    { label: 9, color: "red" },
+    { label: 12, color: "red" },
+    { label: 15, color: "black" },
+    { label: 18, color: "red" },
+    { label: 21, color: "red" },
+    { label: 24, color: "black" },
+    { label: 27, color: "red" },
+    { label: 30, color: "red" },
+    { label: 33, color: "black" },
+    { label: 36, color: "red" },
   ],
   secondRow: [
-    { label: "2", color: "black" },
-    { label: "5", color: "red" },
-    { label: "8", color: "black" },
-    { label: "11", color: "black" },
-    { label: "14", color: "red" },
-    { label: "17", color: "black" },
-    { label: "20", color: "black" },
-    { label: "23", color: "red" },
-    { label: "26", color: "black" },
-    { label: "29", color: "black" },
-    { label: "32", color: "red" },
-    { label: "35", color: "black" },
+    { label: 2, color: "black" },
+    { label: 5, color: "red" },
+    { label: 8, color: "black" },
+    { label: 11, color: "black" },
+    { label: 14, color: "red" },
+    { label: 17, color: "black" },
+    { label: 20, color: "black" },
+    { label: 23, color: "red" },
+    { label: 26, color: "black" },
+    { label: 29, color: "black" },
+    { label: 32, color: "red" },
+    { label: 35, color: "black" },
   ],
   thirdRow: [
-    { label: "1", color: "red" },
-    { label: "4", color: "black" },
-    { label: "7", color: "red" },
-    { label: "10", color: "red" },
-    { label: "13", color: "black" },
-    { label: "16", color: "red" },
-    { label: "19", color: "red" },
-    { label: "22", color: "black" },
-    { label: "25", color: "red" },
-    { label: "28", color: "red" },
-    { label: "31", color: "black" },
-    { label: "34", color: "red" },
+    { label: 1, color: "red" },
+    { label: 4, color: "black" },
+    { label: 7, color: "red" },
+    { label: 10, color: "red" },
+    { label: 13, color: "black" },
+    { label: 16, color: "red" },
+    { label: 19, color: "red" },
+    { label: 22, color: "black" },
+    { label: 25, color: "red" },
+    { label: 28, color: "red" },
+    { label: 31, color: "black" },
+    { label: 34, color: "red" },
   ],
 };
+
+//declaring here all the combinations, easier this way
+export const twoByOneFirst = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36];
+export const twoByOneSecond = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35];
+export const twoByOneThird = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34];
+export const firstTwelves = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+export const secondTwelves = [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+export const thirdTwelves = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36];
+export const oneToEighteen = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+];
+export const nineteenToThirtySix = [
+  19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
+];
+export const black = [
+  2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35,
+];
+export const red = [
+  1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36,
+];
+export const even = [
+  2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36,
+];
+export const odd = [
+  1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35,
+];
