190612beda398de18606588103ab244bb2616ddb
Vasili <vasilisalda0528@outlook.com>
2024-07-20 03:47:42 +0400

    fix winning amount calculate logic

--- src/components/infos/index.js
+++ src/components/infos/index.js
@@ -54,16 +54,24 @@ const Info = () => {
                 />
               </div>
               <div className="hot-cold-numbers">
-                <RouteltteField title="HOT AND COLD NUMBERS" numberInfo="2" />
+                <RouteltteField title="HOT AND COLD NUMBERS" />
               </div>
               <div></div>
             </div>
             <div className="progress-info">
               <div className="progress-box">
-                <RouteltteField title="RED|BLACK" numberInfo="2" />
+                <RouteltteField
+                  title="RED|BLACK"
+                  numberInfo="2"
+                  field={"color"}
+                />
               </div>
               <div className="progress-box">
-                <RouteltteField title="ODD|EVEN" numberInfo="2" />
+                <RouteltteField
+                  title="ODD|EVEN"
+                  numberInfo="2"
+                  field={"even"}
+                />
               </div>
               <div className="progress-box">
                 <InputField />
