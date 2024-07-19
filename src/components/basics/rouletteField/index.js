61f90858568e209e14e5d3f0327e688be64f909c
Vasili <vasilisalda0528@outlook.com>
2024-07-20 03:48:33 +0400

    Update info controls style (latest numbers, red/black, odd/even)

--- src/components/basics/rouletteField/index.js
+++ src/components/basics/rouletteField/index.js
@@ -1,6 +1,21 @@
 /* eslint-disable eqeqeq */
 import React from "react";
+
+import { red, black, even, odd } from "../../../consts/BetTable";
+import useHook from "../../../store/hooks";
+
 import "./index.css";
+
+const DATA_SET = {
+  color: [
+    { color: "#a60000", data: red },
+    { color: "#0b0b0b", data: black },
+  ],
+  even: [
+    { color: "#045a98", data: odd },
+    { color: "#001831", data: even },
+  ],
+};
 
 const RouteltteField = (props) => {
   const {
@@ -10,8 +25,36 @@ const RouteltteField = (props) => {
     numberInfo,
     onChangeCash,
     inputDisabled = true,
+    numberArrays = [],
+    field,
   } = props;
-  const numberArrays = [];
+  const {
+    state: { latestNumbers },
+  } = useHook();
+
+  const ColorSection = ({ data, color }) => {
+    const percent =
+      (latestNumbers.filter(
+        (one) => data.includes(`${one}`) || data.includes(Number(one))
+      ).length /
+        latestNumbers.length) *
+      100;
+
+    if (percent > 0) {
+      return (
+        <div
+          style={{
+            width: `${percent}%`,
+            backgroundColor: color,
+          }}
+        >
+          {Math.round(percent)}%
+        </div>
+      );
+    } else {
+      return <></>;
+    }
+  };
 
   return (
     <div
@@ -32,55 +75,74 @@ const RouteltteField = (props) => {
         </div>
         <div className="info-content">
           <div className="value">
-            <span className="unit">{unit}</span>
-            <div
-              className="info"
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                width: "55%",
-              }}
-            >
-              {numberInfo != 1 ? (
-                <input
-                  type="number"
-                  value={value}
-                  style={{
-                    backgroundColor: "transparent",
-                    color: "#ffffff",
-                    fontWeight: "bold",
-                    border: "none",
-                    width: "90%",
-                    textAlign: "center",
-                  }}
-                  onChange={(e) => {
-                    onChangeCash({
-                      field: "cash",
-                      value: Number(e.target.value),
-                    });
-                  }}
-                  disabled={inputDisabled}
-                />
-              ) : (
-                <>
-                  {numberArrays.map((item) => (
-                    <input
-                      type="text"
-                      value={item}
-                      style={{
-                        backgroundColor: "transparent",
-                        color: "#ffffff",
-                        fontWeight: "bold",
-                        border: "none",
-                        width: "90%",
-                        textAlign: "center",
-                      }}
-                      disabled
+            {numberInfo == 1 ? (
+              <div className="latest-numbers-content">
+                {[0, 0, 0, 0, 0, 0, 0, 0].map((item, index) => (
+                  <div
+                    key={index}
+                    style={{
+                      color: red.includes(Number(numberArrays[index]))
+                        ? "red"
+                        : black.includes(Number(numberArrays[index]))
+                        ? "black"
+                        : "green",
+                    }}
+                  >
+                    {numberArrays[index]}
+                  </div>
+                ))}
+              </div>
+            ) : numberInfo == 2 ? (
+              <div className="number-frequency">
+                {latestNumbers.length > 0 && (
+                  <>
+                    <ColorSection
+                      data={DATA_SET[field][0].data}
+                      color={DATA_SET[field][0].color}
                     />
-                  ))}
-                </>
-              )}
-            </div>
+                    <ColorSection data={["0", "00"]} color={"#426c06"} />
+                    <ColorSection
+                      data={DATA_SET[field][1].data}
+                      color={DATA_SET[field][1].color}
+                    />
+                  </>
+                )}
+              </div>
+            ) : (
+              <>
+                <span className="unit">{unit}</span>
+                <div
+                  className="info"
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    width: "55%",
+                  }}
+                >
+                  <input
+                    type="number"
+                    value={value}
+                    style={{
+                      backgroundColor: "transparent",
+                      color: "#ffffff",
+                      fontWeight: "bold",
+                      border: "none",
+                      width: "90%",
+                      textAlign: "center",
+                      maxHeight: "50%",
+                      margin: "auto",
+                    }}
+                    onChange={(e) => {
+                      onChangeCash({
+                        field: "cash",
+                        value: Number(e.target.value),
+                      });
+                    }}
+                    disabled={inputDisabled}
+                  />
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
