aafb67ec16c9f05e0d3dfdaf229cb8ca15c7fb8b
Vasili <vasilisalda0528@outlook.com>
2024-07-20 16:27:01 +0400

    Add full screen option

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,6 +4,37 @@ import useHook from "../../store/hooks";
 
 import "./styles.css";
 
+function toggleFullScreen(elem) {
+  if (
+    (document.fullScreenElement !== undefined &&
+      document.fullScreenElement === null) ||
+    (document.msFullscreenElement !== undefined &&
+      document.msFullscreenElement === null) ||
+    (document.mozFullScreen !== undefined && !document.mozFullScreen) ||
+    (document.webkitIsFullScreen !== undefined && !document.webkitIsFullScreen)
+  ) {
+    if (elem.requestFullScreen) {
+      elem.requestFullScreen();
+    } else if (elem.mozRequestFullScreen) {
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullScreen) {
+      elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    } else if (elem.msRequestFullscreen) {
+      elem.msRequestFullscreen();
+    }
+  } else {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+  }
+}
+
 const Footer = () => {
   const {
     state: { cash, betsData, winAmount },
