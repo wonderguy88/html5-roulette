b7bc097569b4a3073fc83ad763e062879d93beac
Vasili <vasilisalda0528@outlook.com>
2024-07-20 16:46:10 +0400

    Add full screen option

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,6 +4,37 @@ import useHook from "../../store/hooks";
 
 import "./styles.css";
 
+function toggleFullScreen(elem) {
+  if (
+    (document.fullScreenElement !== undefined &&
+      document.fullScreenElement === null) ||
+    (document.msFullscreenElement !== undefined &&
+      document.msFullscreenElement === null) ||
+    (document.mozFullScreen !== undefined && !document.mozFullScreen) ||
+    (document.webkitIsFullScreen !== undefined && !document.webkitIsFullScreen)
+  ) {
+    if (elem.requestFullScreen) {
+      elem.requestFullScreen();
+    } else if (elem.mozRequestFullScreen) {
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullScreen) {
+      elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    } else if (elem.msRequestFullscreen) {
+      elem.msRequestFullscreen();
+    }
+  } else {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+  }
+}
+
 const Footer = () => {
   const {
     state: { cash, betsData, winAmount },
@@ -15,7 +46,19 @@ const Footer = () => {
         <span data-btn="menu"> </span>
         <span data-btn="son"> </span>
         <span data-btn="red"> </span>
-        <span data-btn="amp"> </span>
+        <span
+          data-btn="amp"
+          onClick={(e) => {
+            toggleFullScreen(document.body);
+            if (e.target.getAttribute("data-btn") == "amp") {
+              e.target.setAttribute("data-btn", "amp-large");
+            } else {
+              e.target.setAttribute("data-btn", "amp");
+            }
+          }}
+        >
+          {" "}
+        </span>
       </div>
       <div className="balance">
         BALANCE <span className="footer-info">${cash}</span>
